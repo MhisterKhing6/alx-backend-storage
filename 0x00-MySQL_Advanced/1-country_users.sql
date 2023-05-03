90cdb9747afd62833f6b9243ff5f3fa82dde7cdd
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 12:54:43 +0000

    how

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Create a table with unique attributes and Enum types
 -- Create a table with enum types
-CREATE TABLE IF NOT EXISTS users
-( id int AUTO_INCREMENT PRIMARY KEY,
-  email varchar(255) NOT NULL UNIQUE,
-  name varchar(255),
-  country ENUM('US', 'CO', 'UN') DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS `users`
+(  `id` int AUTO_INCREMENT PRIMARY KEY,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `name` varchar(255),
+  `country` ENUM('US', 'CO', 'UN') DEFAULT 'US'
  );
