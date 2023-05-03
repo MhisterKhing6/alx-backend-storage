b46694600a991006ef686c6fa36ee4dda5449322
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 13:09:26 +0000

    5

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,12 @@
 -- Creating a triger on orders to update a table on insert
 -- updates the quantity table when oder is inserted
 DELIMITER //;
+-- Create the triger process
 CREATE Trigger `decrease_item` AFTER INSERT ON `orders`
 For EACH ROW
 BEGIN
 UPDATE `items`
+-- Decrease quantity value
 SET `quantity` = `quantity` - `NEW.number`
 WHERE `name` = `NEW.item_name`;
 END;//
