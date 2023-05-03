8e9d3f8b1472fe88abef333fa4627cfc9a0f88b2
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 12:59:18 +0000

    3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
--- order the number of fan from a band --
+-- order the number of fan from a band
+-- Select the orgisins from metal bans
 select `origin` , SUM(`fans`) as `nb_fans` FROM `metal_bands`
 GROUP BY  `origin`
 ORDER BY `nb_fans` DESC;
