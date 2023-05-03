d5d99917d6e322a2b87f13fb4dac8c3670d3de36
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 12:50:17 +0000

    try

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---Create a table with unique attributes
---Create a table with unique attri
+-- Create a table with unique attributes
+-- Create a table with unique attri
 CREATE TABLE IF NOT EXISTS `users`
 ( `id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
   `email` varchar(255) NOT NULL UNIQUE,
