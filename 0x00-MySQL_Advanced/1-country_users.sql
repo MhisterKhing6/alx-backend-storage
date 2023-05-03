6935e4ecf00b60b6f15aa29953a43a68ca339aae
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 12:53:08 +0000

    2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,5 @@
---Create a table with unique attributes and Enum types
+-- Create a table with unique attributes and Enum types
+-- Create a table with enum types
 CREATE TABLE IF NOT EXISTS users
 ( id int AUTO_INCREMENT PRIMARY KEY,
   email varchar(255) NOT NULL UNIQUE,
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
---order the number of fan from a band --
+-- order the number of fan from a band --
 select `origin` , SUM(`fans`) as `nb_fans` FROM `metal_bands`
 GROUP BY  `origin`
 ORDER BY `nb_fans` DESC;
