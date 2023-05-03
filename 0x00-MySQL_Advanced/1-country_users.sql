9d252112a671bcaba187632833d8580b12e4dcdf
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 12:58:00 +0000

    2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS `users`
 (  `id` int AUTO_INCREMENT PRIMARY KEY,
   `email` varchar(255) NOT NULL UNIQUE,
   `name` varchar(255),
-  `country` ENUM('US', 'CO', 'UN') DEFAULT 'US'
+  `country` ENUM("US", "CO", "UN") DEFAULT "US"
  );
