577325d401311bd7904a187307030e9c955aef8f
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-03 13:03:13 +0000

    3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
---order the number of fan from a band --
+-- order the number of fan from a band
+-- select band base on lifespan
 select `band_name` ,(`split` - `formed`) as `lifespan` 
 FROM  `metal_bands`
 WHERE `style`='Glam rock'
