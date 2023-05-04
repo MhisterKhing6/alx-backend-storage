746072665019500f39a19d969ab57a0931c6ff7d
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-04 03:27:04 +0000

    let cc

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,10 +1,13 @@
 -- Adding a new correction procedure
--- Add bonause to correction database
-DELIMITER //;
-CREATE PROCEDURE AddBonus(IN id INT, IN project_name VARCHAR(255), IN score INT)
+-- Add bonause to correction databasei
+DELIMITER $$
+CREATE PROCEDURE `AddBonus`(IN id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-IF `project_name` NOT IN (SELECT `name` FROM `projects`) THEN
+DECLARE pid INT;
+IF project_name NOT IN (SELECT `name` FROM `projects`) THEN
     INSERT INTO projects(name) VALUES (project_name);
 END IF;
-INSERT INTO corrections(`user_id`, `project_id`, `score`) VALUES (`id`, (SELECT id from `projects` WHERE `name` = `project_name`), `score`);
-END; //
+SELECT `id` INTO pid from projects WHERE `name`  = project_name;
+INSERT INTO corrections(`user_id`, `project_id`, `score`) VALUES (id, pid , score);
+END $$
+DELIMITER ; $$
