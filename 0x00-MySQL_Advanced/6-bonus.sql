3251e747d2f82b9714068a5e7095336ddf5f6148
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-04 03:30:46 +0000

    name

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,11 +3,8 @@
 DELIMITER $$
 CREATE PROCEDURE `AddBonus`(IN id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-DECLARE pid INT;
-IF project_name NOT IN (SELECT `name` FROM `projects`) THEN
-    INSERT INTO projects(name) VALUES (project_name);
 END IF;
 SELECT `id` INTO pid from projects WHERE `name`  = project_name;
-INSERT INTO corrections(`user_id`, `project_id`, `score`) VALUES (id, pid , score);
+INSERT INTO `corrections`(`user_id`, `project_id`, `score`) VALUES (id, (SELECT id from `projects` WHERE `name` = project_name;) , score);
 END $$
 DELIMITER ; $$
