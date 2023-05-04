8ecbdb316fa59bf09dad544a54494d903656a8d6
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-04 03:38:49 +0000

    try

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE PROCEDURE `AddBonus`(IN id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
 END IF;
-SELECT `id` INTO pid from projects WHERE `name`  = project_name;
+--Get pid from projects table
 INSERT INTO `corrections`(`user_id`, `project_id`, `score`) VALUES (id, (SELECT id from `projects` WHERE `name` = project_name;) , score);
 END $$
 DELIMITER ; $$
