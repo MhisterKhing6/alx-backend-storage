3736a6c22a578f77e6e21d5dcc381a4cc9f30135
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-09 06:05:31 +0000

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Change the topic of name to to topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update_topics : Update the collections of topics
+    name : The name to change the topic
+    topics : the topic to change the name
+    """
+    return mongo_collection.update(
+        {'name': name}, {'$set': {'topics': topics}}
+        )
