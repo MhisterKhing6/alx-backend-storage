eb01a4c307135b009c22e7f19699d9352b81e19e
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-09 07:21:04 +0000

    123

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,21 +3,25 @@
 Retrieve all school that has a particular topic
 """
 from pymongo import MongoClient
-db = MongoClient('mongodb://127.0.0.1:27017')
-collection = db.logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-number_of_methos = []
-# Get the number of posts in collection
-for method in methods:
-    number_of_methos.append(
-        collection.count_documents({"method": method})
+
+if __name__ == "__main__":
+    db = MongoClient('mongodb://127.0.0.1:27017')
+    collection = db.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    number_of_methos = []
+    # Get the number of posts in collection
+    for method in methods:
+        number_of_methos.append(
+            collection.count_documents({"method": method})
+            )
+    # Get all logs with method equals post and path status
+    get_status = collection.count_documents(
+        {"method": "GET", "path": "/status"}
         )
-# Get all logs with method equals post and path status
-get_status = collection.count_documents({"method": "GET", "path": "/status"})
-count = 0
-print("{} logs".format(sum(number_of_methos)))
-print("Methods:")
-for method in methods:
-    print("\tmethod {}: {}".format(method, number_of_methos[count]))
-    count = count + 1
-print("{} status check".format(get_status))
+    count = 0
+    print("{} logs".format(sum(number_of_methos)))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method, number_of_methos[count]))
+        count = count + 1
+    print("{} status check".format(get_status))
