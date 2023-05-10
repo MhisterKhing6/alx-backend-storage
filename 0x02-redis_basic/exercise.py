3d247698b8e758a65b0b76d5907be94d6a49b7e4
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-10 06:29:06 +0000

    0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Creat a redis class for connection and loading
+"""
+from uuid import uuid4
+from typing import Union
+import redis
+
+
+class Cache(object):
+    """
+    A Class that instantiates Redis instance
+    """
+
+    def __init__(self):
+        """
+        Instantiate instance to a private variable
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        """
+            store: Store data in redis
+            args: data to store
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
